0cb03bd2491b7fb559b0f9a4f8e63913bf2395ba
hellogit <958686274@qq.com>
2022-06-18 18:38:36 +0800

    Initial commit

---
+++ src/main/java/com/a958686274/gulimall/GulimallApplication.java
@@ -0,0 +1,13 @@
+package com.a958686274.gulimall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GulimallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimallApplication.class, args);
+    }
+
+}
